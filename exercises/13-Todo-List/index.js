8401ab93cedfd8ace0c8ce2010313937c64af879
atxoupi <andres.hermelo@gmail.com>
2022-05-23 16:36:39 +0000

    Funcion añadir completa

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,8 @@
 // Your code here
+let input=document.querySelector("#addToDo");
+let ul=document.querySelector("ul");
+input.addEventListener("change",function (){
+  ul.innerHTML+=`<li><span><i class="fa fa-trash"></i></span>${input.value}</li>`;
+});
+
+
