a96f54eb49a60e166ee9885cca53c7404800d562
atxoupi <andres.hermelo@gmail.com>
2022-05-26 14:31:21 +0000

    Ya borra la lista incial, si haces cambios no

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,8 +1,21 @@
 // Your code here
 let input=document.querySelector("#addToDo");
 let ul=document.querySelector("ul");
+let papelera=document.querySelectorAll("span");
+let eliminar=document.querySelectorAll("li");
 input.addEventListener("change",function (){
-  ul.innerHTML+=`<li><span><i class="fa fa-trash"></i></span>${input.value}</li>`;
+  ul.innerHTML+=`<li><span><i class="fa fa-trash"></i></span> ${input.value}</li>`;
 });
 
 
+  for (let i=0;i<eliminar.length;i++){
+    for(let t=0;t<papelera.length;t++){
+      if(t===i){
+        papelera[t].addEventListener("click", function(){
+          console.log(eliminar[i]);
+          ul=document.querySelector("ul");
+          ul.removeChild(eliminar[i]);
+        });
+      }
+    }
+  }
